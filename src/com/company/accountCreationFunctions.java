21518743d001a4ec2866161537b5384bbd301a8a
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-02-22 20:19:27 +0000

    account creation comments

--- src/com/company/accountCreationFunctions.java
+++ src/com/company/accountCreationFunctions.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class accountCreationFunctions { //all needs to be converted from text file storage to database.
+public class accountCreationFunctions {
     public static String currentUsername;
 
     public static void makeUser(){
-        ArrayList<String> newUser = new ArrayList<>();
-        newUser.add(userDetails("user"));
+        ArrayList<String> newUser = new ArrayList<>(); //holds all details for a new account
+        newUser.add(userDetails("user")); //default set to user authority
         try {
             FileWriter myWriter = new FileWriter(Main.users.getName(), true); //True means append to file contents, False means overwrite
             myWriter.write(newUser.get(0) + "\n"); // writes to file
@@ -54,6 +54,8 @@ public class accountCreationFunctions { //all needs to be converted from text fi
         }
     }
 
+
+    //method that actually gets all the details for a new user
     public static String userDetails(String authority) { //prompts for new users username and password
         String username = getUsername("enter username (must be at least 4 characters and contain no spaces)");
         String password = getPassword("enter enter password (must contain at least 1 letter and 1 number and contain no spaces and be at least 6 characters long)");
@@ -63,6 +65,7 @@ public class accountCreationFunctions { //all needs to be converted from text fi
     }
 
 
+    //gets the username input and calls on the checks
     public static String getUsername(String prompt) { //gets input for username and checks it passed parameters
         Scanner input = new Scanner(System.in);
         String strInput = "";
@@ -86,15 +89,16 @@ public class accountCreationFunctions { //all needs to be converted from text fi
     }
 
 
-    public static boolean checkUsername(String username){ //the actual checks for a new username
-        boolean valid = false;
+    //checks if a given username is a valid input (valid being more than 4 characters, no spaces and must be unique)
+    public static boolean checkUsername(String username){
+        boolean valid = false; //validity state of the username
         int checks = 0;
-        if (username.contains(" ")) {
+        if (username.contains(" ")) { //spaces check
             System.out.println("you cant use spaces in your username");
         }else {
             checks++;
         }
-        if (username.length() > 3) {
+        if (username.length() > 3) { //length check
             checks++;
         }else{
             System.out.println("username too short");
@@ -106,7 +110,7 @@ public class accountCreationFunctions { //all needs to be converted from text fi
             String sql = "SELECT username FROM users"; //gets all usernames
             ResultSet rs = stmt.executeQuery(sql); //executes the sql
             while(rs.next()) {
-                if (username.equals(rs.getString("username"))){
+                if (username.equals(rs.getString("username"))){ //checks if username is unique
                     System.out.println("this username is taken");
                     checks--;
                 }
@@ -115,18 +119,18 @@ public class accountCreationFunctions { //all needs to be converted from text fi
             System.out.println("Error in the SQL class: " + e);
         }
         checks++;
-        if (checks == 3) {
+        if (checks == 3) { //looks to see if the username passed all checks
             valid = true;
         }
-        return (valid);
+        return (valid); //returns if the username was valid or not (true = was valid, false = wasnt valid)
     }
 
 
-
-    public static String getPassword(String prompt) { //gets password input form user checks it met parameters then sends it to be hashed
+    //gets password input form user checks it met parameters then sends it to be hashed so it can be compared to the stored value which is hashed
+    public static String getPassword(String prompt) {
         Scanner input = new Scanner(System.in);
         String strInput = "";
-        boolean validPassword = false;
+        boolean validPassword = false; //validity state of password
         while (validPassword == false) {
             try {
                 System.out.println(prompt);
@@ -147,7 +151,8 @@ public class accountCreationFunctions { //all needs to be converted from text fi
     }
 
 
-    public static String passwordHash(String inputPassword){ // puts passwords through MD5 hash
+    // puts passwords through MD5 hash so the text files dont contain the actual passwords to accounts
+    public static String passwordHash(String inputPassword){
         String password = inputPassword;
         String hashedPassword = null;
         try //code for hash algorithm in try catch from online source not made personally * https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
@@ -163,36 +168,37 @@ public class accountCreationFunctions { //all needs to be converted from text fi
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        return(hashedPassword);
+        return(hashedPassword); //returns the password hashed
     }
 
 
-    public static boolean checkPassword(String password){ //runs checks for a valid password
-        boolean valid = false;
-        char a;
+    //the actual checks for a valid password (valid being no spaces, at least 6 characters and made of at least one letter and at least one number)
+    public static boolean checkPassword(String password){
+        boolean valid = false; //validity state of password
+        char a; //temp char holder
         int checks = 0;
-        for (a = 'a'; a <= 'z'; ++a) {
+        for (a = 'a'; a <= 'z'; ++a) { //checking the password has at least one letter
             String strA = String.valueOf(a);
             if (password.contains(strA)) {
                 checks++;
                 break;
             }
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //checking the password has at least one number
             String strA = String.valueOf(i);
             if (password.contains(strA)) {
                 checks++;
                 break;
             }
         }
-        if (password.length() > 5) {
+        if (password.length() > 5) { //checking password length
             checks++;
         }
-        if (password.contains(" ")) {
+        if (password.contains(" ")) { //checking password has no spaces
         }else {
             checks++;
         }
-        if (checks == 4) {
+        if (checks == 4) { //looks to see if password passed all checks
             valid = true;
         }
         return (valid);
